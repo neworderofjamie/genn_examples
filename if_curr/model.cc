8cfffc78258a36a0a8ffb79874bffdb2f3591d9c
jamie <neworderofjamie@gmail.com>
2017-02-07 18:22:05 +0000

    Updated if_curr example to use first pass derived param support

--- if_curr/model.cc
+++ if_curr/model.cc
@@ -3,10 +3,48 @@
 
 #include "modelSpec.h"
 
-class ClosedFormLIF : public dpclass
+//----------------------------------------------------------------------------
+// ClosedFormLIF
+//----------------------------------------------------------------------------
+class ClosedFormLIF : public NeuronModels::BaseSingleton<ClosedFormLIF>
 {
 public:
-    virtual double calculateDerivedParameter(int index, vector<double> pars, double dt = 1.0)
+    DECLARE_PARAM_VALUES(7);
+    DECLARE_INIT_VALUES(2);
+
+    SET_SIM_CODE(
+        "if ($(RefracTime) <= 0.0)\n"
+        "{\n"
+        "  if ($(V) >= $(Vthresh))\n"
+        "  {\n"
+        "    $(V) = $(Vreset);\n"
+        "    $(RefracTime) = $(TauRefrac);\n"
+        "  }\n"
+        "  scalar alpha = (($(Isyn) + $(Ioffset)) * $(Rmembrane)) + $(Vrest);\n"
+        "  $(V) = alpha - ($(ExpTC) * (alpha - $(V)));\n"
+        "}\n"
+        "else\n"
+        "{\n"
+        "  $(RefracTime) -= DT;\n"
+        "}\n"
+    );
+
+    SET_THRESHOLD_CONDITION_CODE("$(RefracTime) <= 0.0 && $(V) >= $(Vthresh)");
+
+    SET_PARAM_NAMES({
+        "C",          // Membrane capacitance
+        "TauM",       // Membrane time constant [ms]
+        "Vrest",      // Resting membrane potential [mV]
+        "Vreset",     // Reset voltage [mV]
+        "Vthresh",    // Spiking threshold [mV]
+        "Ioffset",    // Offset current
+        "TauRefrac"});
+
+    SET_DERIVED_PARAM_NAMES({"ExpTC", "Rmembrane"});
+
+    SET_INIT_VALS({{"V", "scalar"}, {"RefracTime", "scalar"}});
+
+    virtual double CalculateDerivedParam(int index, const vector<double> &pars, double dt = 1.0) const
     {
         switch (index)
         {
@@ -20,59 +58,11 @@ public:
     }
 };
 
-
 void modelDefinition(NNmodel &model)
 {
   initGeNN();
   model.setDT(1.0);
   model.setName("if_curr");
-
-  //---------------------------------------------------------------------------
-  // Create LIF neuron
-  //---------------------------------------------------------------------------
-  const unsigned int MY_LIF = nModels.size();
-  nModels.push_back(neuronModel());
-
-  nModels.back().varNames.reserve(2);
-  nModels.back().varNames = {"V", "RefracTime"};
-
-  nModels.back().varTypes.reserve(2);
-  nModels.back().varTypes = {"scalar", "scalar"};
-
-  nModels.back().pNames.reserve(7);
-  nModels.back().pNames =
-  {
-    "C",          // Membrane capacitance
-    "TauM",       // Membrane time constant [ms]
-    "Vrest",      // Resting membrane potential [mV]
-    "Vreset",     // Reset voltage [mV]
-    "Vthresh",    // Spiking threshold [mV]
-    "Ioffset",    // Offset current
-    "TauRefrac",  // Refractory time [ms]
-  };
-
-  nModels.back().dpNames.reserve(2);
-  nModels.back().dpNames = {"ExpTC", "Rmembrane"};
-  nModels.back().dps = new ClosedFormLIF();
-
-  //TODO: replace the resetting in the following with BRIAN-like threshold and resetting
-  nModels.back().simCode =
-    "if ($(RefracTime) <= 0.0)\n"
-    "{\n"
-    "  if ($(V) >= $(Vthresh))\n"
-    "  {\n"
-    "    $(V) = $(Vreset);\n"
-    "    $(RefracTime) = $(TauRefrac);\n"
-    "  }\n"
-    "  scalar alpha = (($(Isyn) + $(Ioffset)) * $(Rmembrane)) + $(Vrest);\n"
-    "  $(V) = alpha - ($(ExpTC) * (alpha - $(V)));\n"
-    "}\n"
-    "else\n"
-    "{\n"
-    "  $(RefracTime) -= DT;\n"
-    "}\n";
-
-  nModels.back().thresholdConditionCode = "$(RefracTime) <= 0.0 && $(V) >= $(Vthresh)";
 
   //---------------------------------------------------------------------------
   // Create exponential current postsynaptic mechanism
@@ -94,23 +84,8 @@ void modelDefinition(NNmodel &model)
   // Build model
   //---------------------------------------------------------------------------
   // LIF model parameters
-  double lifParams[7] =
-  {
-    1.0,    // 0 - C
-    20.0,   // 1 - TauM
-    -70.0,  // 2 - Vrest
-    -70.0,  // 3 - Vreset
-    -51.0,  // 4 - Vthresh
-    0.0,   // 5 - Ioffset
-    2.0,    // 6 - TauRefrac
-  };
-
-  // LIF initial conditions
-  double lifInit[2] =
-  {
-    -70.0,  // 0 - V
-    0.0,    // 1 - RefracTime
-  };
+  auto lifParamVals = ClosedFormLIF::ParamValues(1.0, 20.0, -70.0, -70.0, -51.0, 0.0, 2.0);
+  auto lifInitVals = ClosedFormLIF::InitValues(-70.0, 0.0);
 
   // Static synapse parameters
   double staticSynapseInit[1] =
@@ -125,9 +100,8 @@ void modelDefinition(NNmodel &model)
   };
 
   // Create IF_curr neuron
-  model.addNeuronPopulation("Stim", 1, SPIKESOURCE, {}, {});
-  model.addNeuronPopulation("Excitatory", 1, MY_LIF,
-                            lifParams, lifInit);
+  model.addNeuronPopulation<NeuronModels::SpikeSource>("Stim", 1, {}, {});
+  model.addNeuronPopulation<ClosedFormLIF>("Excitatory", 1, lifParamVals, lifInitVals);
 
   model.addSynapsePopulation("StimToExcitatory", NSYNAPSE, ALLTOALL, INDIVIDUALG, NO_DELAY, MY_EXP_CURR,
                              "Stim", "Excitatory",
