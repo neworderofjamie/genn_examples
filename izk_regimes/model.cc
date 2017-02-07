72a216fc989f033190a3b167c22c9164c169bc24
jamie <neworderofjamie@gmail.com>
2017-02-07 18:20:12 +0000

    Updated Izk regime example to use new neuron model API

--- izk_regimes/model.cc
+++ izk_regimes/model.cc
@@ -1,5 +1,35 @@
 #include "modelSpec.h"
 
+//----------------------------------------------------------------------------
+// IzhikevichVS
+//----------------------------------------------------------------------------
+class IzhikevichV : public NeuronModels::BaseSingleton<IzhikevichV>
+{
+public:
+    DECLARE_PARAM_VALUES(1);
+    DECLARE_INIT_VALUES(6);
+
+    SET_SIM_CODE(
+        "    if ($(V) >= 30.0){\n"
+        "      $(V)=$(c);\n"
+        "                  $(U)+=$(d);\n"
+        "    } \n"
+        "    $(V)+=0.5*(0.04*$(V)*$(V)+5.0*$(V)+140.0-$(U)+$(Isyn)+$(Ioffset))*DT; //at two times for numerical stability\n"
+        "    $(V)+=0.5*(0.04*$(V)*$(V)+5.0*$(V)+140.0-$(U)+$(Isyn)+$(Ioffset))*DT;\n"
+        "    $(U)+=$(a)*($(b)*$(V)-$(U))*DT;\n"
+        "    $(U)+=$(a)*($(b)*$(V)-$(U))*DT;\n"
+        "   //if ($(V) > 30.0){   //keep this only for visualisation -- not really necessaary otherwise \n"
+        "   //  $(V)=30.0; \n"
+        "   //}\n"
+    );
+
+    SET_THRESHOLD_CONDITION_CODE("$(V) >= 29.99");
+
+    SET_PARAM_NAMES({"Ioffset"});
+
+    SET_INIT_VALS({{"V","scalar"}, {"U", "scalar"}, {"a", "scalar"}, {"b", "scalar"}, {"c", "scalar"}, {"d", "scalar"}});
+};
+
 void modelDefinition(NNmodel &model)
 {
   initGeNN();
@@ -7,39 +37,10 @@ void modelDefinition(NNmodel &model)
   model.setName("izk_regimes");
 
   // Izhikevich model parameters
-  double parameters[1] =
-  {
-    10.0, // 0 - i offset
-  };
-
-  double initialConditions[7] =
-  {
-    -65.0,  // 0 - V
-    -20.0,  // 1 - U
-    0.02,   // 2 - a
-    0.2,    // 3 - b
-    -65.0,  // 4 - c
-    8.0,    // 5 - d
-  };
-
-  // Copy standard Izhikevich neuron and modify simulation code so offset current is applied
-  auto n = nModels[IZHIKEVICH_V];
-  n.pNames.push_back("Ioffset");
-  n.simCode= "    if ($(V) >= 30.0){\n\
-      $(V)=$(c);\n\
-      $(U)+=$(d);\n\
-    } \n\
-    $(V)+=0.5*(0.04*$(V)*$(V)+5.0*$(V)+140.0-$(U)+$(Isyn)+$(Ioffset))*DT; //at two times for numerical stability\n\
-    $(V)+=0.5*(0.04*$(V)*$(V)+5.0*$(V)+140.0-$(U)+$(Isyn)+$(Ioffset))*DT;\n\
-    $(U)+=$(a)*($(b)*$(V)-$(U))*DT;\n\
-    //if ($(V) > 30.0){      //keep this only for visualisation -- not really necessaary otherwise \n\
-    //  $(V)=30.0; \n\
-    //}\n";
-  const unsigned int MY_IZHIKEVICH_V = nModels.size();
-  nModels.push_back(n);
+  auto paramValues = IzhikevichV::ParamValues(10.0);
+  auto initValues = IzhikevichV::InitValues(-65.0, -20.0, 0.02, 0.2, -65.0, 8.0);
 
   // Create population of Izhikevich neurons
-  model.addNeuronPopulation("Neurons", 4, MY_IZHIKEVICH_V,
-                            parameters, initialConditions);
+  model.addNeuronPopulation<IzhikevichV>("Neurons", 4, paramValues, initValues);
   model.finalize();
 }
