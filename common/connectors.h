f24f7ab2d4bdd4f22db1922a650d61f327698c54
jamie <neworderofjamie@gmail.com>
2017-04-24 14:37:02 +0100

    Started moving connector-building code into common

---
+++ common/connectors.h
@@ -0,0 +1,61 @@
+#pragma once
+
+// Standard C++ includes
+#include <algorithm>
+#include <vector>
+
+// GeNN includes
+#include "sparseProjection.h"
+
+//----------------------------------------------------------------------------
+// Typedefines
+//----------------------------------------------------------------------------
+typedef void (*allocateFn)(unsigned int);
+
+//----------------------------------------------------------------------------
+// Functions
+//----------------------------------------------------------------------------
+template <typename Generator>
+void buildFixedProbabilityConnector(unsigned int numPre, unsigned int numPost, float probability,
+                                    SparseProjection &projection, allocateFn allocate, Generator &gen)
+{
+  // Allocate memory for indices
+  // **NOTE** RESIZE as this vector is populated by index
+  std::vector<unsigned int> tempIndInG;
+  tempIndInG.resize(numPre + 1);
+
+  // Reserve a temporary vector to store indices
+  std::vector<unsigned int> tempInd;
+  tempInd.reserve((unsigned int)((float)(numPre * numPost) * probability));
+
+  // Create RNG to draw probabilities
+  std::uniform_real_distribution<> dis(0.0, 1.0);
+
+  // Loop through pre neurons
+  for(unsigned int i = 0; i < numPre; i++)
+  {
+    // Connections from this neuron start at current end of indices
+    tempIndInG[i] = tempInd.size();
+
+    // Loop through post neurons
+    for(unsigned int j = 0; j < numPost; j++)
+    {
+      // If there should be a connection here, add one to temporary array
+      if(dis(gen) < probability)
+      {
+        tempInd.push_back(j);
+      }
+    }
+  }
+
+  // Add final index
+  tempIndInG[numPre] = tempInd.size();
+
+  // Allocate SparseProjection arrays
+  // **NOTE** shouldn't do directly as underneath it may use CUDA or host functions
+  allocate(tempInd.size());
+
+  // Copy indices
+  std::copy(tempIndInG.begin(), tempIndInG.end(), &projection.indInG[0]);
+  std::copy(tempInd.begin(), tempInd.end(), &projection.ind[0]);
+}
--- vogels_2011/simulator.cc
+++ vogels_2011/simulator.cc
@@ -3,56 +3,10 @@
 #include <numeric>
 #include <random>
 
+#include "../common/connectors.h"
 #include "../common/spike_csv_recorder.h"
 
 #include "vogels_2011_CODE/definitions.h"
-
-typedef void (*allocateFn)(unsigned int);
-
-template<typename Generator>
-void build_fixed_probability_connector(unsigned int numPre, unsigned int numPost, float probability,
-                                       SparseProjection &projection, allocateFn allocate, Generator &gen)
-{
-  // Allocate memory for indices
-  // **NOTE** RESIZE as this vector is populated by index
-  std::vector<unsigned int> tempIndInG;
-  tempIndInG.resize(numPre + 1);
-
-  // Reserve a temporary vector to store indices
-  std::vector<unsigned int> tempInd;
-  tempInd.reserve((unsigned int)((float)(numPre * numPost) * probability));
-
-  // Create RNG to draw probabilities
-  std::uniform_real_distribution<> dis(0.0, 1.0);
-
-  // Loop through pre neurons
-  for(unsigned int i = 0; i < numPre; i++)
-  {
-    // Connections from this neuron start at current end of indices
-    tempIndInG[i] = tempInd.size();
-
-    // Loop through post neurons
-    for(unsigned int j = 0; j < numPost; j++)
-    {
-      // If there should be a connection here, add one to temporary array
-      if(dis(gen) < probability)
-      {
-        tempInd.push_back(j);
-      }
-    }
-  }
-
-  // Add final index
-  tempIndInG[numPre] = tempInd.size();
-
-  // Allocate SparseProjection arrays
-  // **NOTE** shouldn't do directly as underneath it may use CUDA or host functions
-  allocate(tempInd.size());
-
-  // Copy indices
-  std::copy(tempIndInG.begin(), tempIndInG.end(), &projection.indInG[0]);
-  std::copy(tempInd.begin(), tempInd.end(), &projection.ind[0]);
-}
 
 int main()
 {
@@ -67,14 +21,14 @@ int main()
   std::random_device rd;
   std::mt19937 gen(rd());
 
-  build_fixed_probability_connector(500, 500, 0.02f,
-                                    CII, &allocateII, gen);
-  build_fixed_probability_connector(500, 2000, 0.02f,
-                                    CIE, &allocateIE, gen);
-  build_fixed_probability_connector(2000, 2000, 0.02f,
-                                    CEE, &allocateEE, gen);
-  build_fixed_probability_connector(2000, 500, 0.02f,
-                                    CEI, &allocateEI, gen);
+  buildFixedProbabilityConnector(500, 500, 0.02f,
+                                 CII, &allocateII, gen);
+  buildFixedProbabilityConnector(500, 2000, 0.02f,
+                                 CIE, &allocateIE, gen);
+  buildFixedProbabilityConnector(2000, 2000, 0.02f,
+                                 CEE, &allocateEE, gen);
+  buildFixedProbabilityConnector(2000, 500, 0.02f,
+                                 CEI, &allocateEI, gen);
 
   // Copy conductances
   std::fill(&gIE[0], &gIE[CIE.connN], 0.0);
