5ac52dda053056f85cced4997eaff4ac322c5453
jamie <neworderofjamie@gmail.com>
2017-10-16 17:34:55 +0100

    * Fixed bug in CPU4->CPU1 connectivity * Tweaked visualization so left and right hemispheres are aligned

--- stone_cx/simulator.cc
+++ stone_cx/simulator.cc
@@ -6,6 +6,7 @@
 
 // Standard C includes
 #include <cmath>
+#include <cstdlib>
 
 // OpenCV includes
 #include <opencv2/opencv.hpp>
@@ -57,16 +58,27 @@ void drawNeuronActivity(scalar activity, const cv::Point &position, F getColourF
 }
 
 template<typename F>
-void drawPopulationActivity(scalar *popActivity, unsigned int popSize, const char *popName,
-                            const cv::Point &position, F getColourFn, cv::Mat &image)
+void drawPopulationActivity(scalar *popActivity, int popSize, const char *popName,
+                            const cv::Point &position, F getColourFn, cv::Mat &image, int numColumns=0)
 {
-    // Draw each neuron's activity
-    for(unsigned int i = 0; i < popSize; i++) {
-        drawNeuronActivity(popActivity[i], position + cv::Point(i * 27, 0), getColourFn, image);
+    // If (invalid) default number of columns is specified, use popsize
+    if(numColumns == 0) {
+        numColumns = popSize;
+    }
+
+    // Loop through each neuron in population
+    for(int i = 0; i < popSize; i++) {
+        // Calculate coordinate in terms of rows and columns
+        auto coord = std::div(i, numColumns);
+        cv::Point offset(coord.rem * 27, coord.quot * 27);
+
+        // Draw neuron activity
+        drawNeuronActivity(popActivity[i], position + offset, getColourFn, image);
     }
 
     // Label population
-    cv::putText(image, popName, position + cv::Point(0, 44),
+    const int numRows = (int)ceil((double)popSize / (double)numColumns);
+    cv::putText(image, popName, position + cv::Point(0, 17 + (27 * numRows)),
                 cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0, CV_RGB(0xFF, 0xFF, 0xFF));
 }
 
@@ -163,7 +175,7 @@ int main()
     std::iota(&CCPU4_CPU1.indInG[0], &CCPU4_CPU1.indInG[Parameters::numCPU4 + 1], 0);
     CCPU4_CPU1.ind[0] = 15;
     for(unsigned int i = 0; i < 7; i++) {
-        CCPU4_CPU1.ind[1 + i] = 7 + i;
+        CCPU4_CPU1.ind[1 + i] = 8 + i;
         CCPU4_CPU1.ind[8 + i] = 1 + i;
     }
     CCPU4_CPU1.ind[15] = 0;
@@ -261,18 +273,18 @@ int main()
                                getBlues, activityImage);
 
         drawPopulationActivity(rTL, Parameters::numTL, "TL", cv::Point(10, 90),
-                               getReds, activityImage);
-        drawPopulationActivity(rCL1, Parameters::numCL1, "CL1", cv::Point(10, 160),
-                               getReds, activityImage);
-        drawPopulationActivity(rTB1, Parameters::numTB1, "TB1", cv::Point(10, 240),
+                               getReds, activityImage, 8);
+        drawPopulationActivity(rCL1, Parameters::numCL1, "CL1", cv::Point(10, 190),
+                               getReds, activityImage, 8);
+        drawPopulationActivity(rTB1, Parameters::numTB1, "TB1", cv::Point(10, 290),
                                getReds, activityImage);
 
-        drawPopulationActivity(rCPU1, Parameters::numCPU1, "CPU1", cv::Point(10, 330),
-                               getGreens, activityImage);
-        drawPopulationActivity(rCPU4, Parameters::numCPU4, "CPU4", cv::Point(10, 410),
-                               getGreens, activityImage);
-        drawPopulationActivity(rPontine, Parameters::numPontine, "Pontine", cv::Point(10, 490),
-                               getGreens, activityImage);
+        drawPopulationActivity(rCPU1, Parameters::numCPU1, "CPU1", cv::Point(10, 390),
+                               getGreens, activityImage, 8);
+        drawPopulationActivity(rCPU4, Parameters::numCPU4, "CPU4", cv::Point(10, 490),
+                               getGreens, activityImage, 8);
+        drawPopulationActivity(rPontine, Parameters::numPontine, "Pontine", cv::Point(10, 590),
+                               getGreens, activityImage, 8);
 
 
         // If we are on outbound segment of route
