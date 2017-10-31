653a40cb1b689df16d1f19bdb5c710afd2d1d29b
jamie <neworderofjamie@gmail.com>
2017-10-31 10:09:07 +0000

    Visual C++ 2013 compatibility - removed constexpr

--- izhikevich_pavlovian/parameters.h
+++ izhikevich_pavlovian/parameters.h
@@ -5,49 +5,49 @@
 //------------------------------------------------------------------------
 namespace Parameters
 {
-    constexpr double timestepMs = 1.0;
+    const double timestepMs = 1.0;
 
     // Simulation duration
-    constexpr double durationMs = 60.0 * 60.0 * 1000.0;
+    const double durationMs = 60.0 * 60.0 * 1000.0;
 
     // How much of start and end of simulation to record
     // **NOTE** we want to see at least one rewarded stimuli in each recording window
-    constexpr double recordStartMs = 40.0 * 1000.0;
-    constexpr double recordEndMs = 40.0 * 1000.0;
+    const double recordStartMs = 40.0 * 1000.0;
+    const double recordEndMs = 40.0 * 1000.0;
 
     // How often should outgoing weights from each synapse be recorded
-    constexpr double weightRecordIntervalMs = durationMs;//10.0 * 1000.0;
+    const double weightRecordIntervalMs = durationMs;//10.0 * 1000.0;
 
     // STDP params
-    constexpr double tauD = 200.0;
+    const double tauD = 200.0;
 
     // scaling
-    constexpr unsigned int sizeScaleFactor = 1;
-    constexpr double weightScaleFactor = 1.0 / (double)sizeScaleFactor;
+    const unsigned int sizeScaleFactor = 1;
+    const double weightScaleFactor = 1.0 / (double)sizeScaleFactor;
 
     // scaled number of cells
-    constexpr unsigned int numExcitatory = 800 * sizeScaleFactor;
-    constexpr unsigned int numInhibitory = 200 * sizeScaleFactor;
-    constexpr unsigned int numCells = numExcitatory + numInhibitory;
+    const unsigned int numExcitatory = 800 * sizeScaleFactor;
+    const unsigned int numInhibitory = 200 * sizeScaleFactor;
+    const unsigned int numCells = numExcitatory + numInhibitory;
 
     // weights
-    constexpr double inhWeight = -1.0 * weightScaleFactor;
-    constexpr double initExcWeight = 1.0 * weightScaleFactor;
-    constexpr double maxExcWeight = 4.0 * weightScaleFactor;
-    constexpr double dopamineStrength = 0.5 * weightScaleFactor;
+    const double inhWeight = -1.0 * weightScaleFactor;
+    const double initExcWeight = 1.0 * weightScaleFactor;
+    const double maxExcWeight = 4.0 * weightScaleFactor;
+    const double dopamineStrength = 0.5 * weightScaleFactor;
 
     // connection probability
-    constexpr double probabilityConnection = 0.1;
+    const double probabilityConnection = 0.1;
 
     // input sets
-    constexpr unsigned int numStimuliSets = 100;
-    constexpr unsigned int stimuliSetSize = 50;
-    constexpr double stimuliCurrent = 40.0;
+    const unsigned int numStimuliSets = 100;
+    const unsigned int stimuliSetSize = 50;
+    const double stimuliCurrent = 40.0;
 
     // regime
-    constexpr double minInterStimuliIntervalMs = 100.0;
-    constexpr double maxInterStimuliIntervalMs = 300.0;
+    const double minInterStimuliIntervalMs = 100.0;
+    const double maxInterStimuliIntervalMs = 300.0;
 
     // reward
-    constexpr double rewardDelayMs = 1000.0;
+    const double rewardDelayMs = 1000.0;
 }
