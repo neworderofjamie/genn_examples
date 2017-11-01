8e66addfa91b75346cd8d4e185dfdaa711ff0e0a
Jamie <neworderofjamie@gmail.com>
2017-11-01 12:32:35 +0000

    added optical flow display

--- camera_360_test/camera_360_test.cc
+++ camera_360_test/camera_360_test.cc
@@ -2,11 +2,36 @@
 #include <iostream>
 
 // Standard C includes
+#include <cassert>
 #include <cmath>
+
+// OpenCV includes
+#include <opencv2/superres/optical_flow.hpp>
 
 // Common includes
 #include "../common/camera_360.h"
 
+void renderOpticalFlow(const cv::Mat &flowX, const cv::Mat &flowY, cv::Mat &flow, int scale)
+{
+    assert(flow.cols == flowX.cols * scale);
+    assert(flow.rows == flowX.rows * scale);
+    
+    flow.setTo(cv::Scalar::all(0));
+    
+    // Loop through output coordinates
+    for(unsigned int x = 0; x < flowX.cols; x++)
+    {
+        for(unsigned int y = 0; y < flowX.rows; y++)
+        {
+            const cv::Point start(x * scale, y * scale);
+            const cv::Point end = start + cv::Point((float)scale * flowX.at<float>(y, x),
+                                                    (float)scale * flowY.at<float>(y, x));
+
+            cv::line(flow, start, end,
+                     CV_RGB(0xFF, 0xFF, 0xFF));
+        }
+    }
+}
 int main(int argc, char *argv[])
 {
     const unsigned int device = (argc > 1) ? std::atoi(argv[1]) : 0;
@@ -23,6 +48,15 @@ int main(int argc, char *argv[])
     cv::namedWindow("Original", CV_WINDOW_NORMAL);
     cv::resizeWindow("Original", 640, 480);
 
+    // Create two grayscale frames
+    cv::Mat frames[2];
+    frames[0].create(10, 90, CV_8UC1);
+    frames[1].create(10, 90, CV_8UC1);
+    
+    cv::Ptr<cv::superres::FarnebackOpticalFlow> opticalFlow = cv::superres::createOptFlow_Farneback();
+    cv::Mat flowX;
+    cv::Mat flowY;
+    cv::Mat flowImage(100, 900, CV_8UC1);
     for(unsigned int i = 0;; i++)
     {
         if(!camera.read()) {
@@ -32,7 +66,21 @@ int main(int argc, char *argv[])
         // Show frame difference
         cv::imshow("Original", camera.getOriginalImage());
         cv::imshow("Unwrapped", camera.getUnwrappedImage());
-
+        
+        // Convert frame to grayscale and store in array
+        const unsigned int currentFrame = i % 2;
+        cv::cvtColor(camera.getUnwrappedImage(), frames[currentFrame], CV_BGR2GRAY);
+        
+        // If this isn't the first frame, calculate optical flow
+        if(i > 0) {
+            const unsigned int prevFrame = (i - 1) % 2;
+            opticalFlow->calc(frames[prevFrame], frames[currentFrame], flowX, flowY);
+            
+            renderOpticalFlow(flowX, flowY, flowImage, 10);
+            
+            cv::imshow("Optical flow", flowImage);
+            //std::cout << flowX.cols << ", " << flowX.rows << ", " << flowX.channels() << std::endl;
+        }
         if(cv::waitKey(1) == 27) {
             break;
         }
