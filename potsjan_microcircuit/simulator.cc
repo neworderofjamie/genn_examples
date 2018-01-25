8e710146529e1e262de57aeee7a8b6667af5356b
jamie <neworderofjamie@gmail.com>
2018-01-25 12:02:53 +0000

    Finer-grained timing

--- potsjan_microcircuit/simulator.cc
+++ potsjan_microcircuit/simulator.cc
@@ -139,23 +139,29 @@ int main()
     ADD_SPIKE_RECORDER(6, E);
     ADD_SPIKE_RECORDER(6, I);
 
+    double simulationMs = 0.0;
+    double pullMs = 0.0;
+    double recordMs = 0.0;
 
+    // Loop through timesteps
+    const unsigned int timesteps = round(Parameters::durationMs / DT);
+    const unsigned int tenPercentTimestep = timesteps / 10;
+    for(unsigned int i = 0; i < timesteps; i++)
     {
-        Timer<> timer("Simulation:");
-        // Loop through timesteps
-        const unsigned int timesteps = round(Parameters::durationMs / DT);
-        const unsigned int tenPercentTimestep = timesteps / 10;
-        for(unsigned int i = 0; i < timesteps; i++)
-        {
-            // Indicate every 10%
-            if((i % tenPercentTimestep) == 0) {
-                std::cout << i / 100 << "%" << std::endl;
-            }
+        // Indicate every 10%
+        if((i % tenPercentTimestep) == 0) {
+            std::cout << i / 100 << "%" << std::endl;
+        }
 
-            // Simulate
+        // Simulate
 #ifndef CPU_ONLY
+        {
+            TimerAccumulate<> timer(simulationMs);
             stepTimeGPU();
+        }
 
+        {
+            TimerAccumulate<> timer(pullMs);
             pull23ECurrentSpikesFromDevice();
             pull23ICurrentSpikesFromDevice();
             pull4ECurrentSpikesFromDevice();
@@ -164,9 +170,16 @@ int main()
             pull5ICurrentSpikesFromDevice();
             pull6ECurrentSpikesFromDevice();
             pull6ICurrentSpikesFromDevice();
+        }
 #else
+            {
+            TimerAccumulate<> timer(simulationMs);
             stepTimeCPU();
+        }
 #endif
+
+        {
+            TimerAccumulate<> timer(recordMs);
 
             // Record spikes
             for(auto &s : spikeRecorders) {
@@ -174,6 +187,10 @@ int main()
             }
         }
     }
+
+    std::cout << "Simulation:" << simulationMs << "ms" << std::endl;
+    std::cout << "Pull from device:" << pullMs << "ms" << std::endl;
+    std::cout << "Record:" << recordMs << "ms" << std::endl;
 
     return 0;
 }
