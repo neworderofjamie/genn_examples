9f987890c190924a54c5b7daeb6d3291348711bf
jamie <neworderofjamie@gmail.com>
2018-02-06 14:00:33 +0000

    Add building of shared library to shell script

--- potsjan_microcircuit/build_simulator_shared_library.sh
+++ potsjan_microcircuit/build_simulator_shared_library.sh
@@ -1,1 +1,4 @@
+pushd potjans_microcircuit_CODE
+make clean all
+popd
 g++ simulator_shared_library.cc -std=c++11 -I $GENN_PATH/lib/include -I $GENN_ROBOTICS_PATH/common -ldl -o simulator_shared_library
