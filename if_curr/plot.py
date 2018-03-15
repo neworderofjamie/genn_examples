d647b4f1ee3272f93d16d7f478b424cf9c4348f6
Jamie <neworderofjamie@gmail.com>
2018-03-15 16:59:45 +0000

    Recording and plotting of input current

--- if_curr/plot.py
+++ if_curr/plot.py
@@ -14,12 +14,17 @@ with open("voltages.csv", "rb") as csv_file:
     # Convert times to numpy
     times = np.asarray(data_columns[0], dtype=float)
     voltage = np.asarray(data_columns[1], dtype=float)
+    current = np.asarray(data_columns[2], dtype=float)
 
     # Create plot
-    figure, axis = plt.subplots()
+    figure, axes = plt.subplots(2)
 
     # Plot voltages
-    axis.plot(times, voltage)
+    axes[0].set_title("Voltage")
+    axes[0].plot(times, voltage)
+    
+    axes[1].set_title("Input current")
+    axes[1].plot(times, current)
 
     # Show plot
     plt.show()
