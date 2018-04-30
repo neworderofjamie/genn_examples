31c5c8a1afc135a7b6424afdff5ddd2b9a2cdf76
jamie <neworderofjamie@gmail.com>
2018-04-30 16:20:52 +0100

    AdExp test

---
+++ adexp_curr/model.cc
@@ -0,0 +1,37 @@
+#include "modelSpec.h"
+
+// GeNN robotics includes
+#include "exp_curr.h"
+#include "adexp.h"
+
+
+void modelDefinition(NNmodel &model)
+{
+    initGeNN();
+    model.setDT(0.1);
+    model.setName("adexp_curr");
+
+    //---------------------------------------------------------------------------
+    // Build model
+    //---------------------------------------------------------------------------
+    // AdExp model parameters
+    AdExp::ParamValues adExpParamVals(281.0,    // Membrane capacitance [pF]
+                                      30.0,     // Leak conductance [nS]
+                                      -70.6,    // Leak reversal potential [mV]
+                                      2.0,      // Slope factor [mV]
+                                      -50.4,    // Threshold voltage [mV]
+                                      10.0,     // Artificial spike height [mV]
+                                      -70.6,    // Reset voltage [mV]
+                                      144.0,    // Adaption time constant
+                                      4.0,      // Subthreshold adaption [nS]
+                                      0.0805,   // Spike-triggered adaptation [nA]
+                                      700.0);   // Offset current
+    
+    AdExp::VarValues adExpInitVals(-70.6,       // 0 - V
+                                   0.0);        // 1 - W
+
+    // Create IF_curr neuron
+    model.addNeuronPopulation<AdExp>("Neurons", 1, adExpParamVals, adExpInitVals);
+    
+    model.finalize();
+}
---
+++ adexp_curr/simulator.cc
@@ -0,0 +1,27 @@
+#include "model.cc"
+#include "adexp_curr_CODE/definitions.h"
+
+int main()
+{
+  allocateMem();
+  initialize();
+
+  FILE *membraneVoltage = fopen("voltages.csv", "w");
+  fprintf(membraneVoltage, "Time(ms), Voltage (mV)\n");
+
+  // Loop through timesteps
+  for(unsigned int i = 0; i < 2000; i++)
+  {
+    // Simulate
+    stepTimeCPU();
+
+    // Calculate simulation time
+    const double time = 1.0 * (double)t;
+    fprintf(membraneVoltage, "%f, %f, %f\n", time, vNeurons[0], wNeurons[0]);
+
+  }
+
+  fclose(membraneVoltage);
+
+  return 0;
+}
