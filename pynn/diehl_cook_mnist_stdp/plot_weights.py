90% sure this was wrong order

pynn/diehl_cook_mnist_stdp/plot_weights.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 weights = np.load("weights.npy")
 
-reshaped_weights = weights.reshape((100, 784))
+reshaped_weights = weights.reshape((784, 100))
 
 fig,axes = plt.subplots(10, 10)
 for i in range(100):
     x = i % 10
     y = i // 10
     a = axes[x,y]
-    rf = reshaped_weights[i,:]
+    rf = reshaped_weights[:,i]
     rf_image = rf.reshape((28,28))
     a.imshow(rf_image)
 plt.show()