8b16aaf60db28e3852cc29e454ba159b7c142dc7
neworderofjamie <neworderofjamie@gmail.com>
2019-12-03 11:43:36 +0000

    Added current sources

--- sudoku/model.cc
+++ sudoku/model.cc
@@ -5,63 +5,80 @@
 #include "puzzles.h"
 
 //----------------------------------------------------------------------------
-// LIFPoisson
+// PoissonCurrentSource
 //----------------------------------------------------------------------------
-//! Leaky integrate-and-fire neuron solved algebraically
-class LIFPoisson : public NeuronModels::Base
+class PoissonCurrentSource : public CurrentSourceModels::Base
 {
 public:
-    DECLARE_MODEL(LIFPoisson, 9, 3);
-
-    SET_SIM_CODE(
+    DECLARE_MODEL(PoissonCurrentSource, 2, 2);
+    
+    SET_PARAM_NAMES({
+        "Tau",
+        "Rate"});
+        
+    SET_VARS({
+        {"Weight", "scalar", VarAccess::READ_ONLY},
+        {"Current", "scalar"}});
+    
+    SET_DERIVED_PARAMS({
+        {"ExpDecay", [](const std::vector<double> &pars, double dt){ return std::exp(-dt / pars[0]); }},
+        {"Init", [](const std::vector<double> &pars, double dt){ return (1.0 - std::exp(-dt / pars[0])) * (pars[0] / dt); }},
+        {"ExpMinusLambda", [](const std::vector<double> &pars, double dt){ return std::exp(-(pars[1] / 1000.0) * dt); }}});
+        
+    SET_INJECTION_CODE(
         "scalar p = 1.0f;\n"
         "unsigned int numPoissonSpikes = 0;\n"
         "do\n"
         "{\n"
         "    numPoissonSpikes++;\n"
         "    p *= $(gennrand_uniform);\n"
-        "} while (p > $(PoissonExpMinusLambda));\n"
-        "$(Ipoisson) += $(IpoissonInit) * (scalar)(numPoissonSpikes - 1);\n"
-        "if ($(RefracTime) <= 0.0)\n"
-        "{\n"
-        "  scalar alpha = (($(Isyn) + $(Ioffset) + $(Ipoisson)) * $(Rmembrane)) + $(Vrest);\n"
-        "  $(V) = alpha - ($(ExpTC) * (alpha - $(V)));\n"
-        "}\n"
-        "else\n"
-        "{\n"
-        "  $(RefracTime) -= DT;\n"
-        "}\n"
-        "$(Ipoisson) *= $(IpoissonExpDecay);\n"
-    );
-
-    SET_THRESHOLD_CONDITION_CODE("$(RefracTime) <= 0.0 && $(V) >= $(Vthresh)");
-
-    SET_RESET_CODE(
-        "$(V) = $(Vreset);\n"
-        "$(RefracTime) = $(TauRefrac);\n");
-
-    SET_PARAM_NAMES({
-        "C",                // Membrane capacitance
-        "TauM",             // Membrane time constant [ms]
-        "Vrest",            // Resting membrane potential [mV]
-        "Vreset",           // Reset voltage [mV]
-        "Vthresh",          // Spiking threshold [mV]
-        "TauRefrac",        // Refractory time [ms]
-        "PoissonWeight",    // How much current each poisson spike adds [nA]
-        "IpoissonTau",      // Time constant of poisson spike integration [ms]
-        "Ioffset"});        // Offset current
-
-    SET_DERIVED_PARAMS({
-        {"ExpTC", [](const std::vector<double> &pars, double dt){ return std::exp(-dt / pars[1]); }},
-        {"Rmembrane", [](const std::vector<double> &pars, double){ return  pars[1] / pars[0]; }},
-        {"IpoissonExpDecay", [](const std::vector<double> &pars, double dt){ return std::exp(-dt / pars[7]); }},
-        {"IpoissonInit", [](const std::vector<double> &pars, double dt){ return pars[6] * (1.0 - std::exp(-dt / pars[7])) * (pars[7] / dt); }}});
-
-    SET_EXTRA_GLOBAL_PARAMS({{"PoissonExpMinusLambda",  "scalar"}});    // Lambda for Poisson process
-
-    SET_VARS({{"V", "scalar"}, {"RefracTime", "scalar"}, {"Ipoisson", "scalar"}});
+        "} while (p > $(ExpMinusLambda));\n"
+        "$(Current) += $(Weight) * $(Init) * (scalar)(numPoissonSpikes - 1);\n"
+        "$(injectCurrent, $(Current));\n"
+        "$(Current) *= $(ExpDecay);\n");
 };
-IMPLEMENT_MODEL(LIFPoisson);
+IMPLEMENT_MODEL(PoissonCurrentSource);
+
+//----------------------------------------------------------------------------
+// CluePoissonCurrentSource
+//----------------------------------------------------------------------------
+class CluePoissonCurrentSource : public CurrentSourceModels::Base
+{
+public:
+    DECLARE_MODEL(CluePoissonCurrentSource, 4, 2);
+    
+    SET_PARAM_NAMES({
+        "Tau",
+        "Rate",
+        "Clue",
+        "CoreSize"});
+        
+    SET_VARS({
+        {"Weight", "scalar", VarAccess::READ_ONLY}, 
+        {"Current", "scalar"}});
+    
+    SET_DERIVED_PARAMS({
+        {"ExpDecay", [](const std::vector<double> &pars, double dt){ return std::exp(-dt / pars[0]); }},
+        {"Init", [](const std::vector<double> &pars, double dt){ return (1.0 - std::exp(-dt / pars[0])) * (pars[0] / dt); }},
+        {"ExpMinusLambda", [](const std::vector<double> &pars, double dt){ return std::exp(-(pars[1] / 1000.0) * dt); }}});
+        
+    SET_INJECTION_CODE(
+        "const unsigned int coreSize = (unsigned int)$(CoreSize);\n"
+        "const unsigned int clue = ((unsigned int)$(Clue) - 1);\n"
+        "if(($(id) / coreSize) == clue) {\n"
+        "    scalar p = 1.0f;\n"
+        "    unsigned int numPoissonSpikes = 0;\n"
+        "    do\n"
+        "    {\n"
+        "        numPoissonSpikes++;\n"
+        "        p *= $(gennrand_uniform);\n"
+        "    } while (p > $(ExpMinusLambda));\n"
+        "    $(Current) += $(Weight) * $(Init) * (scalar)(numPoissonSpikes - 1);\n"
+        "    $(injectCurrent, $(Current));\n"
+        "    $(Current) *= $(ExpDecay);\n"
+        "}\n");
+};
+IMPLEMENT_MODEL(CluePoissonCurrentSource);
 
 //----------------------------------------------------------------------------
 // DomainToDomain
@@ -138,27 +155,47 @@ void buildModel(ModelSpec &model, const Puzzle<S> &puzzle)
         -55.0); // 1 - max
     
     // Parameters for LIF neurons
-    LIFPoisson::ParamValues lifParams(
+    NeuronModels::LIF::ParamValues lifParams(
         0.25,   // Membrane capacitance
         20.0,   // Membrane time constant [ms]
         -65.0,  // Resting membrane potential [mV]
         -70.0,  // Reset voltage [mV]
         -50.0,  // Spiking threshold [mV]
-        2.0,    // Refractory time [ms]
-        0.0,    // How much current each poisson spike adds [nA]
-        10.0,      // Time constant of poisson spike integration [ms]
-        0.3);
-    
+        0.3,    // Offset current [nA]
+        2.0);   // Refractory time [ms]
+
     // Initial values for LIF neurons
-    LIFPoisson::VarValues lifInit(
+    NeuronModels::LIF::VarValues lifInit(
         initVar<InitVarSnippet::Uniform>(vDist),    // V
-        0.0,                                        // RefracTime
-        0.0);                                       // Ipoisson);   // V
+        0.0);                                       // RefracTime
     
     // Parameters for exponentially-shaped synapses
     PostsynapticModels::ExpCurr::ParamValues expCurrParams(
         5.0);   // Tau
+    
+    // Distribution of weights for noise input
+    InitVarSnippet::Uniform::ParamValues stimWeightDist(
+        1.4,  // 0 - min
+        1.6); // 1 - max
         
+    PoissonCurrentSource::ParamValues stimParams(
+        5.0,    // Tau [ms]
+        20.0);  // Rate [Hz]
+    
+    PoissonCurrentSource::VarValues stimInitVals(
+        initVar<InitVarSnippet::Uniform>(stimWeightDist),   // Weight [nA]
+        0.0);                                               // Current [nA]
+    
+    // Distribution of weights for clue noise input
+    InitVarSnippet::Uniform::ParamValues clueWeightDist(
+        1.8,  // 0 - min
+        2.0); // 1 - max
+    
+    // Initial values for clue noise input
+    CluePoissonCurrentSource::VarValues clueStimInitVals(
+        initVar<InitVarSnippet::Uniform>(clueWeightDist),   // Weight
+        0.0);                                               // Current [nA]
+    
     // Add neuron populations for each variable state
     //sudoku.build_domains_pops()
     // sudoku.build_stimulation_pops(1, shrink=1.0,stim_ratio=1.,rate=(20.0, 20.0),full=True, phase=0.0, clue_size=None))
@@ -167,14 +204,30 @@ void buildModel(ModelSpec &model, const Puzzle<S> &puzzle)
     // sudoku.build_dissipation_pops(d_populations=1, shrink=1.0, stim_ratio=1.0, rate=20.0, full=True, phase=0.0)
     // > dissipative poisson for each neuron (random start and stop) - SEEMS disabled
     // sudoku.stimulate_cores(w_range=[1.4, 1.6], d_range=[1.0, 1.0], w_clues=[1.8, 2.0]) # , w_clues=[1.5, 1.9])
-    
+
     for(size_t y = 0; y < S; y++) {
         for(size_t x = 0; x < S; x++) {
-            auto *neuronPop = model.addNeuronPopulation<LIFPoisson>(Parameters::getPopName(x, y), Parameters::coreSize * 9, lifParams, lifInit);
+            // Create neuron population
+            const std::string popName = Parameters::getPopName(x, y);
+            auto *neuronPop = model.addNeuronPopulation<NeuronModels::LIF>(popName, Parameters::coreSize * 9, lifParams, lifInit);
             neuronPop->setSpikeLocation(VarLocation::HOST_DEVICE);
-            
-            // If this variable state is a clue
+        
+            // If this variable state is a clue, add a permanent Poisson current input to strongly excite it
             if(puzzle.puzzle[y][x] != 0) {
+                // Parameters for clue noise input
+                CluePoissonCurrentSource::ParamValues clueStimParams(
+                    5.0,                    // Tau [ms]
+                    20.0,                   // Rate [Hz]
+                    puzzle.puzzle[y][x],    // Clue
+                    Parameters::coreSize);  // Core size
+                    
+                model.addCurrentSource<CluePoissonCurrentSource>("clue_" + popName, popName, 
+                                                                 clueStimParams, clueStimInitVals);
+            }
+            // Otherwise, add a weaker Poisson current input
+            else {
+                model.addCurrentSource<PoissonCurrentSource>("stim_" + popName, popName, 
+                                                             stimParams, stimInitVals);
             }
         }
     }
