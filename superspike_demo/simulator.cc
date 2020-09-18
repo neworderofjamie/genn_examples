f4a3be5d03a9d6238b97308f58c89e901dfa2636
neworderofjamie <J.C.Knight@sussex.ac.uk>
2020-09-18 17:21:45 +0100

    start of ras loader

--- superspike_demo/simulator.cc
+++ superspike_demo/simulator.cc
@@ -1,6 +1,9 @@
 // Standard C++ includes
+#include <fstream>
 #include <iostream>
 #include <random>
+#include <string>
+#include <sstream>
 
 // GeNN userproject includes
 #include "timer.h"
@@ -15,10 +18,47 @@
 // Auto-generated model code
 #include "superspike_demo_CODE/definitions.h"
 
+namespace
+{
+void stripWindowsLineEnding(std::string &lineString)
+{
+    // If line has a Windows line ending, remove it
+    if(!lineString.empty() && lineString.back() == '\r') {
+        lineString.pop_back();
+    }
+}
+void loadSpikes(const std::string &filename)
+{
+    // Open ras file
+    std::ifstream rasFile(filename);
+    if(!rasFile.good()) {
+        throw std::runtime_error("Cannot open ras file: " + filename);
+    }
+
+    // Read lines into strings
+    std::vector<std::pair<unsigned int, double>> data;
+    std::string lineString;
+    while(std::getline(rasFile, lineString)) {
+        // Strip windows line endings
+        stripWindowsLineEnding(lineString);
+
+        // Wrap line in stream for easier parsing
+        std::istringstream lineStream(lineString);
+
+        // Add new tuple to vector and read line into it
+        data.emplace_back();
+        lineStream >> data.back().second;
+        lineStream >> data.back().first;
+
+        std::cout << data.back().first << "," << data.back().second << std::endl;
+    }
+}
+}   // Anonymous namespace
 int main()
 {
     try
     {
+        loadSpikes("oxford-target.ras");
         allocateMem();
         initialize();
 
@@ -29,7 +69,7 @@ int main()
         initializeSparse();
 
 
-        float epsilon = 0.003f;
+        float epsilon = 0.000000000000000000001f;
         {
             Timer a("Simulation wall clock:");
 
@@ -37,19 +77,19 @@ int main()
             for(unsigned int trial = 0; trial < Parameters::numTrials; trial++) {
                 if((trial % 100) == 0) {
                     // if this isn't the first trial, reduce learning rate
-                    if(trial != 0) {
+                    /*if(trial != 0) {
                         learningRate *= 0.7f;
-                    }
+                    }*/
 
-                    std::cout << "Trial " << trial << " (learning rate " << learningRate << ")" << std::endl;
+                    std::cout << "Trial " << trial << " (epsilon " << epsilon << ")" << std::endl;
                 }
 
                 // Loop through timesteps within trial
-                for(unsigned int i = 0; i < trialTimesteps; i++) {
+                for(unsigned int i = 0; i < Parameters::trialTimesteps; i++) {
                     stepTime();
 
                     // If it's time to update weights
-                    if((iT % updateTimesteps) == 0) {
+                    if((iT % Parameters::updateTimesteps) == 0) {
                         BatchLearning::rMaxPropCUDA(d_mInput_Hidden, d_upsilonInput_Hidden, d_wInput_Hidden,
                                                  Parameters::numInput, Parameters::numHidden,
                                                  Parameters::updateTimeMs, Parameters::tauRMS, Parameters::r0, epsilon, Parameters::wMin, Parameters::wMax);
@@ -59,6 +99,7 @@ int main()
                     }
 
                 }
+            }
 
         }
         // Open CSV output files
@@ -86,5 +127,9 @@ int main()
         std::cout << "Synapse dynamics:" << synapseDynamicsTime << std::endl;
 
         return EXIT_SUCCESS;
+    }
+    catch(std::exception &ex) {
+        std::cerr << ex.what() << std::endl;
+        return EXIT_FAILURE;
     }
 }
