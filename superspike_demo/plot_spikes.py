0d25910f882811869bbc3072977e23126e8ca3c4
neworderofjamie <J.C.Knight@sussex.ac.uk>
2020-10-02 17:20:06 +0100

    slightly improved plotting

--- superspike_demo/plot_spikes.py
+++ superspike_demo/plot_spikes.py
@@ -4,19 +4,24 @@ import numpy as np
 
 
 spike_dtype = {"names": ("time", "neuron_id"), "formats": (np.float, np.int)}
-# Create plot
-figure, axes = plt.subplots(2, 5)
+spike_times = list(range(0, 600, 100))
 
-for i in range(5):
+# Create plot
+figure, axes = plt.subplots(3, len(spike_times), sharex=True)
+
+for i, t in enumerate(spike_times):
     # Read spikes
-    input_spikes = np.loadtxt("input_spikes_%u.csv" % i, delimiter=",", skiprows=1,
+    input_spikes = np.loadtxt("input_spikes_%u.csv" % t, delimiter=",", skiprows=1,
                               dtype=spike_dtype)
-    output_spikes = np.loadtxt("output_spikes_%u.csv" % i, delimiter=",", skiprows=1,
+    hidden_spikes = np.loadtxt("hidden_spikes_%u.csv" % t, delimiter=",", skiprows=1,
+                               dtype=spike_dtype)
+    output_spikes = np.loadtxt("output_spikes_%u.csv" % t, delimiter=",", skiprows=1,
                                dtype=spike_dtype)
 
     # Plot spikes
     axes[0, i].scatter(input_spikes["time"], input_spikes["neuron_id"], s=2, edgecolors="none")
-    axes[1, i].scatter(output_spikes["time"], output_spikes["neuron_id"], s=2, edgecolors="none")
+    axes[1, i].scatter(hidden_spikes["time"], hidden_spikes["neuron_id"], s=2, edgecolors="none")
+    axes[2, i].scatter(output_spikes["time"], output_spikes["neuron_id"], s=2, edgecolors="none")
 
 
     #axis.set_ylabel("Neuron number")
