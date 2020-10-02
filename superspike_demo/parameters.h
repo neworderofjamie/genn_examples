e5b0b51cf4e7184a2b03fb239d950a25c81db35b
neworderofjamie <J.C.Knight@sussex.ac.uk>
2020-10-02 17:19:55 +0100

    tweaked parameters to match original

--- superspike_demo/parameters.h
+++ superspike_demo/parameters.h
@@ -14,14 +14,15 @@ namespace Parameters
     constexpr double tauDecay = 10.0;
     constexpr double tauRMS = 30000.0;
     constexpr double r0 = 0.005;
-    constexpr double epsilon = 0.000000000000000000001;
-    constexpr double wMin = -10.0;
-    constexpr double wMax = 10.0;
+    constexpr double epsilon = 1E-12;
+    constexpr double wMin = -0.1;
+    constexpr double wMax = 0.1;
+    constexpr double w0 = 0.05;
 
     // Experiment parameters
     constexpr double inputFreqHz = 5.0;
-    constexpr unsigned int numTrials = 1500;
-    constexpr double updateTimeMs = 10000.0;
+    constexpr unsigned int numTrials = 600;
+    constexpr double updateTimeMs = 5000.0;
     constexpr double trialMs = 1890.0;
 
     // Convert parameters to timesteps
