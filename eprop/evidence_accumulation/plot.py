96e9280338560674f4c80f538108ef739c1bf05f
neworderofjamie <neworderofjamie@gmail.com>
2020-10-28 13:41:24 +0000

    plotting work: * switch to pandas for faster loading of csv * calculate correct trials per epoch

--- eprop/evidence_accumulation/plot.py
+++ eprop/evidence_accumulation/plot.py
@@ -1,21 +1,47 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from pandas import read_csv
 
-spike_dtype = {"names": ("time", "neuron_id"), "formats": (np.float, np.int)}
-
-input_spikes = np.loadtxt("input_spikes.csv", delimiter=",", skiprows=1, dtype=spike_dtype)
-recurrent_lif_spikes = np.loadtxt("recurrent_lif_spikes.csv", delimiter=",", skiprows=1, dtype=spike_dtype)
-recurrent_alif_spikes = np.loadtxt("recurrent_alif_spikes.csv", delimiter=",", skiprows=1, dtype=spike_dtype)
-
-output_data = np.loadtxt("output.csv", delimiter=",",
-                         dtype={"names": ("time", "pi1", "pi2", "pi_star1", "pi_star2"),
-                                "formats": (np.float, np.float, np.float, np.float, np.float)})
+input_spikes = read_csv("input_spikes.csv", header=None, names=["time", "neuron_id"], skiprows=1, delimiter=",",
+                        dtype={"time":float, "neuron_id":int})
+recurrent_lif_spikes = read_csv("recurrent_lif_spikes.csv", header=None, names=["time", "neuron_id"], skiprows=1, delimiter=",",
+                                dtype={"time":float, "neuron_id":int})
+recurrent_alif_spikes = read_csv("recurrent_alif_spikes.csv", header=None, names=["time", "neuron_id"], skiprows=1, delimiter=",",
+                                 dtype={"time":float, "neuron_id":int})
+output_data = read_csv("output.csv", header=None, names=["time", "pi1", "pi2", "pi_star1", "pi_star2"], skiprows=1, delimiter=",",
+                        dtype={"time":float, "pi1":float, "pi2":float, "pi_star1":float, "pi_star2":float})
 times = np.loadtxt("times.csv")
-#epsilon_data = np.loadtxt("epsilon.csv", delimiter=",",
-#                         dtype={"names": ("time", "epsilon_0", "epsilon_1", "epsilon_2", "psi_0", "psi_1", "psi_2", "v0", "v1", "v2", "a0", "a1", "a2"),
-#                                "formats": (np.float, np.float, np.float, np.float, np.float, np.float, np.float, np.float, np.float, np.float, np.float, np.float, np.float)})
 
+# When decisions are made, pi* is increased above zero
+decision_mask = (output_data["pi_star1"] >= 0.0)
+
+# Extract output data during decision time and reshape so each row contains a single trials decision
+# **NOTE** for some reason reshape doesn't work correctly on pandas data frames
+pi1_output = np.reshape(np.asarray(output_data["pi1"][decision_mask]), (-1, 150))
+pi2_output = np.reshape(np.asarray(output_data["pi2"][decision_mask]), (-1, 150))
+pi_star1_output = np.reshape(np.asarray(output_data["pi_star1"][decision_mask]), (-1, 150))
+pi_star2_output = np.reshape(np.asarray(output_data["pi_star2"][decision_mask]), (-1, 150))
+
+# Calculate sum of each output for each trial
+pi1_output = np.sum(pi1_output, axis=1)
+pi2_output = np.sum(pi2_output, axis=1)
+pi_star1_output = np.sum(pi_star1_output, axis=1)
+pi_star2_output = np.sum(pi_star2_output, axis=1)
+
+# Stack outputs together into 2 row matrixes
+pi_output = np.vstack((pi1_output, pi2_output))
+pi_star_output = np.vstack((pi_star1_output, pi_star2_output))
+
+# Find largest output for each trial
+pi_decision = np.argmax(pi_output, axis=0)
+pi_star_decision = np.argmax(pi_star_output, axis=0)
+
+# Count correct decisions per epoch
+correct_decision = (pi_decision == pi_star_decision)
+correct_decision = np.reshape(correct_decision, (-1, 64))
+correct_decision = np.sum(correct_decision, axis=1)
+print(correct_decision)
 
 # Create plot
 figure, axes = plt.subplots(5, sharex=True)
@@ -45,26 +71,6 @@ axes[4].plot(output_data["time"], output_data["pi_star2"], label="$\pi^*_2$")
 axes[4].axhline(0.5, linestyle="--", color="gray")
 axes[4].vlines(times, 0.0, 1.0, linestyle="--", color="gray")
 axes[4].legend(loc="upper right")
-"""
-axes[4].set_title("Epsilon")
-axes[4].plot(epsilon_data["time"], epsilon_data["epsilon_0"])
-axes[4].plot(epsilon_data["time"], epsilon_data["epsilon_1"])
-axes[4].plot(epsilon_data["time"], epsilon_data["epsilon_2"])
 
-axes[5].set_title("Psi")
-axes[5].plot(epsilon_data["time"], epsilon_data["psi_0"])
-axes[5].plot(epsilon_data["time"], epsilon_data["psi_1"])
-axes[5].plot(epsilon_data["time"], epsilon_data["psi_2"])
-
-axes[6].set_title("V")
-axes[6].plot(epsilon_data["time"], epsilon_data["v0"])
-axes[6].plot(epsilon_data["time"], epsilon_data["v1"])
-axes[6].plot(epsilon_data["time"], epsilon_data["v2"])
-
-axes[7].set_title("A")
-axes[7].plot(epsilon_data["time"], epsilon_data["a0"])
-axes[7].plot(epsilon_data["time"], epsilon_data["a1"])
-axes[7].plot(epsilon_data["time"], epsilon_data["a2"])
-"""
 # Show plot
 plt.show()
