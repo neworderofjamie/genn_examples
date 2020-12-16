d392cee83288e7c207720eafea20a379cf0203bd
neworderofjamie <neworderofjamie@gmail.com>
2020-12-16 17:47:02 +0000

    new input model to turn mnist->smnist on GPU

---
+++ eprop/s_mnist/parameters.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Parameters
+{
+    constexpr double timestepMs = 1.0;
+    constexpr bool timingEnabled = true;
+
+    constexpr unsigned int numInputNeurons = 100;
+    constexpr unsigned int numRecurrentNeurons = 1000;
+    constexpr unsigned int numOutputNeurons = 10;
+}
