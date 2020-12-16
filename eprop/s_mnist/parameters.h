a6174ab9e3d654f2efbbc3b958cfe587c6b10ff2
neworderofjamie <neworderofjamie@gmail.com>
2020-12-16 18:25:31 +0000

    project and basic simulator with MNIST loader

--- eprop/s_mnist/parameters.h
+++ eprop/s_mnist/parameters.h
@@ -5,6 +5,12 @@ namespace Parameters
     constexpr double timestepMs = 1.0;
     constexpr bool timingEnabled = true;
 
+    constexpr unsigned int inputWidth = 28;
+    constexpr unsigned int inputHeight = 28;
+    constexpr unsigned int inputRepeats = 2;
+    constexpr unsigned int cueDuration = 20;
+    constexpr unsigned int trialTimesteps = (inputWidth * inputHeight * inputRepeats) + cueDuration;
+
     constexpr unsigned int numInputNeurons = 100;
     constexpr unsigned int numRecurrentNeurons = 1000;
     constexpr unsigned int numOutputNeurons = 10;
---
+++ eprop/s_mnist/simulator.cc
@@ -0,0 +1,170 @@
+// Standard C++ includes
+#include <fstream>
+#include <iostream>
+
+// GeNN userproject includes
+#include "analogueRecorder.h"
+#include "spikeRecorder.h"
+
+// Auto-generated model code
+#include "s_mnist_CODE/definitions.h"
+
+// Batch-learning includes
+#include "batch_learning.h"
+
+// Model parameters
+#include "parameters.h"
+
+//----------------------------------------------------------------------------
+// Anonynous namespace
+//----------------------------------------------------------------------------
+namespace
+{
+uint32_t readBigEndian(std::ifstream &data)
+{
+    union
+    {
+        char b[4];
+        uint32_t w;
+    } swizzle;
+
+    // Read data into swizzle union
+    data.read(&swizzle.b[0], 4);
+
+    // Swap endianess
+    std::swap(swizzle.b[0], swizzle.b[3]);
+    std::swap(swizzle.b[1], swizzle.b[2]);
+    return swizzle.w;
+}
+
+unsigned int loadImageData(const std::string &imageDatafilename, uint8_t *&egp,
+                           void (*allocateEGPFn)(unsigned int), void (*pushEGPFn)(unsigned int))
+{
+    // Open binary file
+    std::ifstream imageData(imageDatafilename, std::ifstream::binary);
+
+    // Read header words
+    const uint32_t magic = readBigEndian(imageData);
+    const uint32_t numImages = readBigEndian(imageData);
+    const uint32_t numRows = readBigEndian(imageData);
+    const uint32_t numCols = readBigEndian(imageData);
+
+    // Validate header words
+    assert(magic == 0x803);
+    assert(numRows == Parameters::inputHeight);
+    assert(numCols == Parameters::inputWidth);
+
+    // Allocate EGP for data
+    allocateEGPFn(numRows * numCols * numImages);
+
+    // Read data into EGP
+    imageData.read(reinterpret_cast<char *>(egp), numRows * numCols * numImages);
+
+    // Push EGP
+    pushEGPFn(numRows * numCols * numImages);
+
+    return numImages;
+}
+
+void loadLabelData(const std::string &labelDataFilename, unsigned int desiredNumLabels, uint8_t *&egp,
+                   void (*allocateEGPFn)(unsigned int), void (*pushEGPFn)(unsigned int))
+{
+    // Open binary file
+    std::ifstream labelData(labelDataFilename, std::ifstream::binary);
+
+    // Read header words
+    const uint32_t magic = readBigEndian(labelData);
+    const uint32_t numLabels = readBigEndian(labelData);
+
+    // Validate header words
+    assert(magic == 0x801);
+    assert(numLabels == desiredNumLabels);
+
+    // Allocate EGP for data
+    allocateEGPFn(numLabels);
+
+    // Read data into EGP
+    labelData.read(reinterpret_cast<char *>(egp), numLabels);
+
+    // Push EGP
+    pushEGPFn(numLabels);
+}
+}   // Anonymous namespace
+
+int main()
+{
+    try
+    {
+        allocateMem();
+        initialize();
+
+        const unsigned int numTrainingImages = loadImageData("mnist/train-images.idx3-ubyte", datasetInput, &allocatedatasetInput, &pushdatasetInputToDevice);
+        //loadLabelData("mnist/train-labels.idx1-ubyte", )
+        
+        // Use CUDA to calculate initial transpose of feedforward recurrent->output weights
+        BatchLearning::transposeCUDA(d_gRecurrentLIFOutput, d_gOutputRecurrentLIF, 
+                                     Parameters::numRecurrentNeurons, Parameters::numOutputNeurons);
+        BatchLearning::transposeCUDA(d_gRecurrentALIFOutput, d_gOutputRecurrentALIF, 
+                                     Parameters::numRecurrentNeurons, Parameters::numOutputNeurons);
+        initializeSparse();
+
+        AnalogueRecorder<float> outputRecorder("output.csv", {PiOutput, PiStarOutput}, Parameters::numOutputNeurons, ",");
+
+        std::ofstream performance("performance.csv");
+        performance << "Epoch, Number of cues, Number correct" << std::endl;
+    
+        float learningRate = 0.005f;
+
+        // Loop through epochs
+        for(unsigned int epoch = 0;; epoch++) {
+            std::cout << "Epoch " << epoch << std::endl;
+
+            // Loop through trials
+            //unsigned int numCorrect = 0;
+            for(unsigned int trial = 0; trial < 64; trial++) {
+                for(unsigned int timestep = 0; timestep < Parameters::trialTimesteps; timestep++) {
+                    stepTime();
+    
+                    // Record output
+                    pullPiStarOutputFromDevice();
+                    pullPiOutputFromDevice();
+                    outputRecorder.record(t);
+                }
+            }
+
+            
+            // Update weights
+            #define ADAM_OPTIMIZER_CUDA(POP_NAME, NUM_SRC_NEURONS, NUM_TRG_NEURONS)   BatchLearning::adamOptimizerCUDA(d_DeltaG##POP_NAME, d_M##POP_NAME, d_V##POP_NAME, d_g##POP_NAME, NUM_SRC_NEURONS, NUM_TRG_NEURONS, epoch, learningRate)
+             
+            ADAM_OPTIMIZER_CUDA(InputRecurrentLIF, Parameters::numInputNeurons, Parameters::numRecurrentNeurons);
+            ADAM_OPTIMIZER_CUDA(InputRecurrentALIF, Parameters::numInputNeurons, Parameters::numRecurrentNeurons);
+            ADAM_OPTIMIZER_CUDA(LIFLIFRecurrent, Parameters::numRecurrentNeurons, Parameters::numRecurrentNeurons);
+            ADAM_OPTIMIZER_CUDA(ALIFLIFRecurrent, Parameters::numRecurrentNeurons, Parameters::numRecurrentNeurons);
+            ADAM_OPTIMIZER_CUDA(LIFALIFRecurrent, Parameters::numRecurrentNeurons, Parameters::numRecurrentNeurons);
+            ADAM_OPTIMIZER_CUDA(ALIFALIFRecurrent, Parameters::numRecurrentNeurons, Parameters::numRecurrentNeurons);
+
+            BatchLearning::adamOptimizerTransposeCUDA(d_DeltaGRecurrentLIFOutput, d_MRecurrentLIFOutput, d_VRecurrentLIFOutput, d_gRecurrentLIFOutput, d_gOutputRecurrentLIF, 
+                                                      Parameters::numRecurrentNeurons, Parameters::numOutputNeurons, 
+                                                      epoch, learningRate);
+            BatchLearning::adamOptimizerTransposeCUDA(d_DeltaGRecurrentALIFOutput, d_MRecurrentALIFOutput, d_VRecurrentALIFOutput, d_gRecurrentALIFOutput, d_gOutputRecurrentALIF, 
+                                                      Parameters::numRecurrentNeurons, Parameters::numOutputNeurons, 
+                                                      epoch, learningRate);
+                                                          
+            // Update biases
+            BatchLearning::adamOptimizerCUDA(d_DeltaBOutput, d_MOutput, d_VOutput, d_BOutput,
+                                             Parameters::numOutputNeurons, 1,
+                                             epoch, learningRate);
+             
+            // Display performance in this epoch
+            //std::cout << "\t" << numCorrect << "/64 correct" << std::endl;
+            
+            // Write performance to file
+            //performance << epoch << ", " << numCues << ", " << numCorrect << std::endl;
+        }
+    }
+    catch(std::exception &ex) {
+        std::cerr << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
