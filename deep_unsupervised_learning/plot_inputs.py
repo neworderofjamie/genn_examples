35e348b1aa8e3d9789967731b0a0edc6ebdf9551
neworderofjamie <neworderofjamie@gmail.com>
2021-03-31 16:21:43 +0100

    script for plotting inputs

---
+++ deep_unsupervised_learning/plot_inputs.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from glob import glob
+
+# Get list of input spike trains
+input_spikes = list(sorted(glob("input_spikes_*.csv"), 
+                           key=lambda n: int(n.split("_")[2].split(".")[0])))
+
+num_rows = int(np.sqrt(len(input_spikes)))
+num_cols = int(np.ceil(len(input_spikes) / float(num_rows)))
+
+fig, axes = plt.subplots(num_rows, num_cols, sharex="col", sharey="row")
+
+for i, c in enumerate(input_spikes):
+    # Load spikes
+    spikes = np.loadtxt(c, skiprows=1, delimiter=",")
+    
+    # Count spikes for each pixel
+    spike_counts = np.bincount(spikes[:,1].astype(int), minlength=28 * 28)
+    spike_counts = np.reshape(spike_counts, (28, 28))
+    
+    axes[i // num_cols,i % num_cols].imshow(spike_counts, cmap="jet")
+
+plt.show()
+    
