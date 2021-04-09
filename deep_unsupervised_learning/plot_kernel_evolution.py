fe8ac14580a7a21fea9e7d3184641e35015dbcc2
neworderofjamie <neworderofjamie@gmail.com>
2021-04-09 08:59:14 +0100

    improve kernel evolution plot

--- deep_unsupervised_learning/plot_kernel_evolution.py
+++ deep_unsupervised_learning/plot_kernel_evolution.py
@@ -2,22 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from glob import glob
 
+def get_time(name):
+    return int(name.split("_")[2].split(".")[0])
+    
 # Get list of kernels
-conv1_kernels = list(sorted(glob("conv1_kernel_*.bin"), 
-                            key=lambda n: int(n.split("_")[2].split(".")[0])))
+conv1_kernels = list(sorted(glob("conv1_kernel_*.bin"), key=get_time))
 
-conv1_kernels = conv1_kernels[::10]
+fig, axes = plt.subplots(16, len(conv1_kernels), sharex="col", sharey="row")
 
-fig, axes = plt.subplots(len(conv1_kernels), 16, sharex="col", sharey="row")
-
-for i, c in enumerate(conv1_kernels):
+for j, c in enumerate(conv1_kernels):
     # Load and reshape kernel
     kernel = np.fromfile(c, dtype=np.float32)
     kernel = np.reshape(kernel, (5, 5, 16))
     
     # Plot seperate channels
-    for j in range(16):
-        axes[i,j].imshow(kernel[:,:,j], cmap="Greys")
+    for i in range(16):
+        axes[i,j].imshow(kernel[:,:,i], cmap="jet")
+        axes[i,j].get_xaxis().set_visible(False)
+        axes[i,j].get_yaxis().set_visible(False)
+    
+    axes[0,j].set_title(get_time(c))
 
 plt.show()
     
