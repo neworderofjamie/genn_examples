ba346e8006770cd490bad5c9e484a68052afa180
neworderofjamie <neworderofjamie@gmail.com>
2021-04-12 16:08:38 +0100

    limit to history of kernels to show

--- deep_unsupervised_learning/plot_kernel_evolution.py
+++ deep_unsupervised_learning/plot_kernel_evolution.py
@@ -4,13 +4,16 @@ from glob import glob
 
 def get_time(name):
     return int(name.split("_")[2].split(".")[0])
-    
+   
+max_kernels = 32
+
 # Get list of kernels
 conv1_kernels = list(sorted(glob("conv1_kernel_*.bin"), key=get_time))
 
-fig, axes = plt.subplots(16, len(conv1_kernels), sharex="col", sharey="row")
+fig, axes = plt.subplots(16, len(conv1_kernels[-max_kernels:]), sharex="col", sharey="row",
+                         gridspec_kw = {"wspace":0, "hspace":0})
 
-for j, c in enumerate(conv1_kernels):
+for j, c in enumerate(conv1_kernels[-max_kernels:]):
     # Load and reshape kernel
     kernel = np.fromfile(c, dtype=np.float32)
     kernel = np.reshape(kernel, (5, 5, 16))
