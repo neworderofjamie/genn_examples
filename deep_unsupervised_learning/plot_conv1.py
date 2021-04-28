f2bbb98bb055c3bd038380dbb358050e7ddafc5c
neworderofjamie <J.C.Knight@sussex.ac.uk>
2021-04-28 13:17:53 +0100

    added CLI to spike plotting script

--- deep_unsupervised_learning/plot_conv1.py
+++ deep_unsupervised_learning/plot_conv1.py
@@ -1,36 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 from glob import glob
 from mpl_toolkits.mplot3d import Axes3D
 
-WIDTH = 28
-HEIGHT = 28
-NUM_CHANNELS = 1
-"""
-WIDTH = 24
-HEIGHT = 24
-NUM_CHANNELS = 16
-"""
-# Load spikes
-#conv_spikes = np.loadtxt("conv1_spike_events_1900.csv", skiprows=1, delimiter=",")
-conv_spikes = np.loadtxt("input_spikes_1900.csv", skiprows=1, delimiter=",")
 
-print("%u spikes" % conv_spikes.shape[0])
+hyper_params = {"input": (28, 28, 1),
+                "conv1": (24, 24, 16),
+                "conv2": (8, 8, 32)}
+
+# Get layer name
+layer = sys.argv[1] if len(sys.argv) > 1 else "input"
+suffix = sys.argv[2] if len(sys.argv) > 2 else "spikes"
+timestep = int(sys.argv[3]) if len(sys.argv) > 3 else 0
+
+# Pull corresponding hyperparameters from dictionary
+width, height, num_channels = hyper_params[layer]
+
+# Load spikes
+spikes = np.loadtxt("%s_%s_%d.csv" % (layer, suffix, timestep), skiprows=1, delimiter=",")
+
+
+print("%u spikes" % spikes.shape[0])
 
 # Convert neuron IDs to row, col and channel
-conv_spike_row = (conv_spikes[:,1] // NUM_CHANNELS) // WIDTH
-conv_spike_col = (conv_spikes[:,1] // NUM_CHANNELS) % WIDTH
-conv_spike_chan = conv_spikes[:,1] % NUM_CHANNELS
+spike_row = (spikes[:,1] // num_channels) // width
+spike_col = (spikes[:,1] // num_channels) % width
+spike_chan = spikes[:,1] % num_channels
 
-hist,_ = np.histogramdd((conv_spike_row, conv_spike_col, conv_spikes[:,0]), bins=(HEIGHT, WIDTH, 1000))
+hist,_ = np.histogramdd((spike_row, spike_col, spikes[:,0]), bins=(height, width, 1000))
 print("Mean:%f, Std:%f, Max:%f" % (np.average(hist), np.std(hist), np.amax(hist)))
-print(np.bincount(conv_spike_chan.astype(int)))
+print(np.bincount(spike_chan.astype(int)))
 # Create 3D plot
 fig = plt.figure()
 axis = fig.add_subplot(111, projection="3d")
 
-axis.scatter(conv_spike_col, conv_spikes[:,0], conv_spike_row, c=conv_spike_chan)
+axis.scatter(spike_col, spikes[:,0], spike_row, c=spike_chan)
 axis.set_xlabel("X")
 axis.set_ylabel("time")
 axis.set_zlabel("Y")
