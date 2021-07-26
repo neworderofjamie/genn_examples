41504a73ce3998e24a0c98f8ec65bdf00df0b93a
neworderofjamie <neworderofjamie@gmail.com>
2021-07-26 13:14:41 +0100

    fixed plotting script to work with parallel batches

--- eprop/plot.py
+++ eprop/plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import os
 import numpy as np
 from pandas import read_csv
 from argparse import ArgumentParser
@@ -7,48 +8,41 @@ from argparse import ArgumentParser
 # Build command line parse
 parser = ArgumentParser(description="Train eProp classifier")
 parser.add_argument("--dt", type=float, default=1.0)
+parser.add_argument("--feedforward", action="store_true")
 parser.add_argument("--batch-size", type=int, default=512)
 parser.add_argument("--num-recurrent-alif", type=int, default=256)
 parser.add_argument("--dataset", choices=["smnist", "shd"], required=True)
 parser.add_argument("--epoch", type=int, default=0)
 parser.add_argument("--batch", type=int, default=0)
 parser.add_argument("--trial", type=int, action="append", required=True)
+parser.add_argument("--suffix", default="")
 args = parser.parse_args()
 
-MAX_STIMULI_TIMES = {"smnist": 1568.0, "shd": 1369.140625}
+MAX_STIMULI_TIMES = {"smnist": 1568.0 * 2.0, "shd": 1369.140625 * 2.0}
 
 # Build file suffix
-name_suffix = "%u" % (args.num_recurrent_alif)
+name_suffix = "%u%s%s" % (args.num_recurrent_alif, "_feedforward" if args.feedforward else "", args.suffix)
+output_directory = "%s_%s" % (args.dataset, name_suffix)
 
-# Load data
-input_spikes = read_csv("%s_input_spikes_%s_%u_%u.csv" % (args.dataset, name_suffix, args.epoch, args.batch), header=None, names=["time", "neuron_id"], skiprows=1, delimiter=",",
-                        dtype={"time":float, "neuron_id":int})
-recurrent_alif_spikes = read_csv("%s_recurrent_spikes_%s_%u_%u.csv" % (args.dataset, name_suffix, args.epoch, args.batch), header=None, names=["time", "neuron_id"], skiprows=1, delimiter=",",
-                                 dtype={"time":float, "neuron_id":int})
 
 # Create plot
 figure, axes = plt.subplots(2, len(args.trial), sharex="col", sharey="row")
 
 # Loop through trials we want to plot
 for i, trial in enumerate(args.trial):
-    # Convert trial and batch into index into epoch
-    epoch_index = (args.batch * args.batch_size) + trial
-
-    # Calculate start and end time of this trial
-    trial_start_time = trial * MAX_STIMULI_TIMES[args.dataset]
-    trial_end_time = trial_start_time + MAX_STIMULI_TIMES[args.dataset]
-
-    # Extract data from this trial
-    trial_input_spikes = input_spikes[(input_spikes["time"] >= trial_start_time) & (input_spikes["time"] < trial_end_time)]
-    trial_recurrent_alif_spikes = recurrent_alif_spikes[(recurrent_alif_spikes["time"] >= trial_start_time) & (recurrent_alif_spikes["time"] < trial_end_time)]
+    # Load data
+    input_spikes = read_csv(os.path.join(output_directory, "input_spikes_%u_%u_%u.csv" % (args.epoch, args.batch, i)), header=None, names=["time", "neuron_id"], skiprows=1, delimiter=",",
+                            dtype={"time":float, "neuron_id":int})
+    recurrent_alif_spikes = read_csv(os.path.join(output_directory, "recurrent_spikes_%u_%u_%u.csv" % (args.epoch, args.batch, i)), header=None, names=["time", "neuron_id"], skiprows=1, delimiter=",",
+                                     dtype={"time":float, "neuron_id":int})
 
     # Plot input spikes
-    axes[0, i].scatter(trial_input_spikes["time"], trial_input_spikes["neuron_id"], s=1, edgecolors="none")
+    axes[0, i].scatter(input_spikes["time"], input_spikes["neuron_id"], s=1, edgecolors="none")
 
     # Plot ALIF spikes
-    axes[1, i].scatter(trial_recurrent_alif_spikes["time"], trial_recurrent_alif_spikes["neuron_id"], s=2, edgecolors="none")
+    axes[1, i].scatter(recurrent_alif_spikes["time"], recurrent_alif_spikes["neuron_id"], s=2, edgecolors="none")
     
-    axes[0, i].set_xlim((trial_start_time, trial_end_time))
+    axes[0, i].set_xlim((0.0, MAX_STIMULI_TIMES[args.dataset]))
 
     if i == 0:
         axes[0, i].set_ylabel("Input\nneuron")
