0fa1021c2e318cb5485834725efb35e49b2a0a95
neworderofjamie <neworderofjamie@gmail.com>
2022-01-06 18:06:12 +0000

    use dictionary parameters

--- va_benchmark/model.cc
+++ va_benchmark/model.cc
@@ -16,22 +16,21 @@ void modelDefinition(NNmodel &model)
     //---------------------------------------------------------------------------
     // Build model
     //---------------------------------------------------------------------------
-    InitVarSnippet::Uniform::ParamValues vDist(
-        Parameters::resetVoltage,       // 0 - min
-        Parameters::thresholdVoltage);  // 1 - max
+    Snippet::ParamValues vDist{
+        {"min", Parameters::resetVoltage},
+        {"max", Parameters::thresholdVoltage}};
 
-    InitSparseConnectivitySnippet::FixedProbability::ParamValues fixedProb(
-        Parameters::probabilityConnection); // 0 - prob
+    Snippet::ParamValues fixedProb{{"prob", Parameters::probabilityConnection}};
 
     // LIF model parameters
-    NeuronModels::LIF::ParamValues lifParams(
-        1.0,    // 0 - C
-        20.0,   // 1 - TauM
-        -49.0,  // 2 - Vrest
-        Parameters::resetVoltage,  // 3 - Vreset
-        Parameters::thresholdVoltage,  // 4 - Vthresh
-        0.0,    // 5 - Ioffset
-        5.0);    // 6 - TauRefrac
+    Snippet::ParamValues lifParams{
+        {"C", 1.0},
+        {"TauM", 20.0},
+        {"Vrest", -49.0},
+        {"Vreset", Parameters::resetVoltage},
+        {"Vthresh", Parameters::thresholdVoltage},
+        {"Ioffset", 0.0},
+        {"TauRefrac", 5.0}};
 
     // LIF initial conditions
     NeuronModels::LIF::VarValues lifInit(
@@ -46,11 +45,8 @@ void modelDefinition(NNmodel &model)
         Parameters::inhibitoryWeight);    // 0 - Wij (nA)
 
     // Exponential current parameters
-    PostsynapticModels::ExpCurr::ParamValues excitatoryExpCurrParams(
-        5.0);  // 0 - TauSyn (ms)
-
-    PostsynapticModels::ExpCurr::ParamValues inhibitoryExpCurrParams(
-        10.0);  // 0 - TauSyn (ms)
+    Snippet::ParamValues excitatoryExpCurrParams{{"tau", 5.0}};
+    Snippet::ParamValues inhibitoryExpCurrParams{{"tau", 10.0}};
 
     // Create IF_curr neuron
     auto *e = model.addNeuronPopulation<NeuronModels::LIF>("E", Parameters::numExcitatory, lifParams, lifInit);
